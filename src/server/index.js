9d625534aee9639f2824762e2ab59ae428c5e433
Jonny Wyatt <jonnywyatt@yahoo.co.uk>
2019-08-11 22:36:19 +0100

    Don't hardcode page routes in Express

--- src/server/index.js
+++ src/server/index.js
@@ -4,7 +4,7 @@ import { routeHandler } from './route-handler';
 const app = express();
 app
   .disable('x-powered-by')
-  .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
-  .get(/\/(overview|places-to-stay|things-to-do)/, routeHandler);
+  .use('/static', express.static(process.env.RAZZLE_PUBLIC_DIR))
+  .get('*', routeHandler);
 
 export default app;
